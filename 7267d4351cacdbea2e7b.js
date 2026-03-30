353734b437f2fe677f1eb94555944838fe276f1d
mohamedhassan20000 <dr.jhrawy@gmail.com>
2026-03-30 16:17:03 +0300

    First Commit

---
+++ 7267d4351cacdbea2e7b.js
@@ -0,0 +1,105 @@
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
+import '@fortawesome/fontawesome-free/js/all.min';
+
+document.querySelectorAll('[data-bs-toggle="tooltip]').forEach(item => new bootstrap.Tooltip(item));
+
+document.querySelectorAll('.add-to-cart-btn').forEach(item => {
+    item.addEventListener("click", () => {
+        alert("تم إضافة المنتج إلى عربة الشراء")
+    });
+});
+
+document.querySelectorAll('.size-option input[type="radio"]').forEach(item => {
+    item.addEventListener('change', () => {
+        document.querySelectorAll('.size-option').forEach(i => {
+            i.classList.remove('active')
+        });
+        item.parentNode.parentNode.classList.add('active')
+    });
+});
+
+document.querySelectorAll('.color-option input[type="radio"]').forEach(item => {
+    item.addEventListener('change', () => {
+        document.querySelectorAll('.color-option').forEach(i => {
+            i.classList.remove('active')
+        });
+        item.parentNode.parentNode.classList.add('active')
+    });
+});
+
+
+//حساب إجمالي سعر المنتج
+document.querySelectorAll('[data-product-quantity]').forEach(item => {
+    item.addEventListener('change', () => {
+        const newQuantity = item.value;
+        const parent = item.closest('[data-product-info]');
+        const pricePerUnit = parent.getAttribute('data-product-price');
+        const totalPriceForProduct = newQuantity * pricePerUnit
+        parent.querySelector('.total-price-for-product').innerHTML = totalPriceForProduct + "$";
+
+        calculateTotalPrice();
+    });
+});
+
+document.querySelectorAll('[data-remove-from-card]').forEach(item => {
+    item.addEventListener('click', () => {
+        item.closest('[data-product-info]').remove()
+        calculateTotalPrice();
+    });
+});
+
+function calculateTotalPrice() {
+    let totalPriceForAllProduct = 0;
+        document.querySelectorAll('[data-product-info]').forEach(product => {
+            const pricePerUnite = product.getAttribute('data-product-price');
+            const quantity = product.querySelector('[data-product-quantity]').value
+            const totalPriceForProduct = pricePerUnite * quantity 
+
+            totalPriceForAllProduct = totalPriceForAllProduct + totalPriceForProduct
+        });
+        document.getElementById('total-price-for-all-product').innerHTML = totalPriceForAllProduct + '$'
+}
+
+//نهاية إجمالي سعر المنتج
+
+
+//اختيار المدينة
+
+const ciyiesByCountry = {
+    sa: [ 'الرياض','جدة','مكة المكرمة','المدينة المنورة','الدمام','الخبر',
+        'الظهران','الطائف','تبوك','بريدة','خميس مشيط','أبها','حائل','نجران',
+        'الجبيل','ينبع','القطيف','الأحساء','عرعر','سكاكا','جازان','القنفذة',
+        'بيشة','الرس','عنيزة'],
+
+    eg: ['القاهرة','الجيزة','الإسكندرية','بورسعيد','السويس','دمياط','المنصورة',
+        'الزقازيق','طنطا','دمنهور','بنها','شبين الكوم','كفر الشيخ','الفيوم',
+        'بني سويف','المنيا','أسيوط','سوهاج','قنا','الأقصر','أسوان','الإسماعيلية',
+        'العريش','مرسى مطروح','الغردقة','شرم الشيخ','الطور','الخارجة' ],
+
+    jo: ['عمان','الزرقاء','إربد','العقبة','السلط','المفرق','مأدبا','الكرك',
+        'جرش','عجلون','الطفيلة','معان','الرمثا'],
+
+    sy: ['دمشق','حلب','حمص','حماة','اللاذقية','طرطوس','دير الزور','الرقة','الحسكة',
+        'إدلب','السويداء','درعا','القامشلي','تدمر','جبلة','بانياس'],
+
+    kw: ['مدينة الكويت','حولي','السالمية','الفروانية','الجهراء','الأحمدي',
+        'مبارك الكبير','الفحيحيل','خيطان','الرميثية','بيان','المنقف',
+        'صباح السالم','العارضية'],
+
+    tr: ['إسطنبول','أنقرة','إزمير','بورصة','أنطاليا','أضنة','قونية','غازي عنتاب',
+        'قيصري','مرسين','ديار بكر','سامسون','شانلي أورفا','إسكي شهير','طرابزون',
+        'أرضروم','ملاطية','وان','صقاريا','قوجه إيلي','موغلا',
+        'دنيزلي',
+        'مرمريس',
+        'بودروم',
+        'ألانيا'
+    ]
+};
+}
+
+
+// copyright
+document.getElementById("copyright").innerHTML = "جميع الحقوق محفوظة للمتجر سنة " + new Date().getFullYear()
+// enf of copyright
---
+++ baf94c1a0c85a2e11ea5.js
@@ -0,0 +1,20 @@
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
+import '@fortawesome/fontawesome-free/js/all.min';
+
+document.querySelectorAll('[data-bs-toggle="tooltip]').forEach(item => new bootstrap.Tooltip(item));
+
+document.querySelectorAll('.add-to-cart-btn').forEach(item => {
+    item.addEventListener("click", () => {
+        alert("تم إضافة المنتج إلى عربة الشراء")
+    });
+});
+
+document.getElementById("copyright").innerHTML = "جميع الحقوق محفوظة للمتجر سنة " + new Date().getFullYear()
+
+document.querySelectorAll(".product-option input")
+
+console.log("أهلا بك في متجر عربي")
+
+console.log("أهلا بك في أكادمية حسوب")
